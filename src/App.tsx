539c102ca456c63a7fce7909bf30084720bc42b6
Aristidas Jasudas <wwwwjas@gmail.com>
2023-01-09 01:57:33 +0200

    changed structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,22 @@ import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 import { Login } from "./pages/Login";
 import { PageNotFound } from "./pages/PageNotFound";
+import { ResetPassword } from "./pages/ResetPassword";
 import { Signup } from "./pages/Signup";
 
 function App() {
   return (
-    <div className="dark h-full">
+    <div className="dark min-h-screen flex flex-col ">
       <BrowserRouter>
         <Header />
-        <main>
-          <Routes>
-            <Route path="/signup" element={<Login />}></Route>
-            <Route path="/login" element={<Signup />}></Route>
-            <Route path="*" element={<PageNotFound />}></Route>
-          </Routes>
-        </main>
+
+        <Routes>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/reset-password" element={<ResetPassword />}></Route>
+          <Route path="*" element={<PageNotFound />}></Route>
+        </Routes>
+
         <Footer />
       </BrowserRouter>
     </div>
